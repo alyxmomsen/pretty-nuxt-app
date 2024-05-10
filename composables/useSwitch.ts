f0d423d8bdd043607a86ff850f52b06bb3067ecb
alex <shizofranky@gmail.com>
2024-05-10 16:09:25 +0300

    refactor

--- composables/useSwitch.ts
+++ composables/useSwitch.ts
@@ -1,6 +1,6 @@
 export default function foo () {
 
     const baseURL = ref(true ?  "https://epress-rest-server-ror-pretty-nuxt-app.vercel.app" : "http://localhost:3001")  ;
-    
+
     return baseURL ;
 }
